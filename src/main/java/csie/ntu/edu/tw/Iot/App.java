f8259084225bc8a40d7498727bbb15c2780b8942
hrbq <zeroblakejack@gmail.com>
2016-12-27 19:50:58 +0800

    init commit

---
+++ src/main/java/csie/ntu/edu/tw/Iot/App.java
@@ -0,0 +1,13 @@
+package csie.ntu.edu.tw.Iot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
